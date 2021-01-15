9a6815132370da8c86315cc74d044a79b8b362eb
rodriV <utndavid@gmail.com>
2021-01-15 00:05:02 -0300

    REFACTOR: configura las coordenadas y la ubicacion de locationfound

--- visualizacionBase/src/public/js/main.js
+++ visualizacionBase/src/public/js/main.js
@@ -3,3 +3,18 @@ const map=L.map('map-template').setView([-31.428366, -64.184616],13);
 //L.tileLayer('https://tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png').addTo(map);
 
 L.tileLayer('http://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png http://b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png').addTo(map);
+
+// OPCION B L.tileLayer('http://a.tile.stamen.com/toner/{z}/{x}/{y}.png').addTo(map);
+map.locate({enableHighAccuracy:true});
+map.on('locationfound', e =>{
+  const coords = [e.latlng.lat, e.latlng.lng];
+  const marker = L.marker(coords);
+  marker.bindPopup('Técnico');
+
+  map.addLayer(marker);
+});
+
+const marker = L.marker([-31.428366, -64.184616]);
+marker.bindPopup('Técnico');
+
+map.addLayer(marker);
